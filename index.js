313627ef7841d8ad7769d1779b84394a7fac831b
Corey Butler <corey@coreybutler.com>
2017-02-11 23:26:37 -0600

    Updated dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('ngnx-data-proxy-database')
+
 /**
  * @class NGNX.DATA.FileProxy
  * Persist NGN DATA stores to a local file. This class was designed to be
@@ -7,7 +9,7 @@
  * common functionality relative to working with the file system, but specific
  * file storage logic should reside in a more specific class.
  */
-class FileProxy extends NGN.DATA.Proxy {
+class FileProxy extends NGNX.DATA.DatabaseProxy {
   constructor (config) {
     if (typeof config === 'string') {
       config = {
@@ -36,22 +38,6 @@ class FileProxy extends NGN.DATA.Proxy {
        * Path to the JSON file.
        */
       dbfile: NGN.private(config.file),
-
-      /**
-       * @cfg {string} [encryptionKey=null]
-       * Set this to a hash key to obfuscate (scramble) the data. This is a
-       * reversible hashing method and should not be considered "secure", but it
-       * will make the file on disk unreadable to a human if they do not have
-       * the key.
-       */
-      munge: NGN.private(NGN.coalesce(config.encryptionKey, null)),
-
-      /**
-       * @cfg {string} [cipher=aes-256-cbc]
-       * The type of cipher to use when encrypting/decrypting data at rest.
-       * This is only applied if #encryptionKey is provided.
-       */
-      cipher: NGN.privateconst(NGN.coalesce(config.cipher, 'aes-256-cbc')),
 
       /**
        * @cfgproperty {boolean} [autolock=true]
@@ -146,34 +132,6 @@ class FileProxy extends NGN.DATA.Proxy {
 
     this.mkdirp(require('path').join(dir, '..'))
     this.mkdirp(dir)
-  }
-
-  /**
-   * @method encrypt
-   * Encrypt text using the configured #cipher and #encryptionKey.
-   * @param {string} decryptedContent
-   * The content to be encrypted.
-   * @private
-   */
-  encrypt (data) {
-    let cipher = require('crypto').createCipher(this.cipher, this.munge)
-    let encoded = cipher.update(data, 'utf8', 'hex')
-    encoded += cipher.final('hex')
-    return encoded
-  }
-
-  /**
-   * @method decrypt
-   * Decrypt text using the configured #cipher and #encryptionKey.
-   * @param {string} encryptedContent
-   * The content to be decrypted.
-   * @private
-   */
-  decrypt (data) {
-    let cipher = require('crypto').createDecipher(this.cipher, this.munge)
-    let decoded = cipher.update(data, 'hex', 'utf8')
-    decoded += cipher.final('utf8')
-    return decoded
   }
 
   /**
@@ -370,41 +328,6 @@ class FileProxy extends NGN.DATA.Proxy {
 
     // Create the output directory if it doesn't already exist.
     this.mkdirp(require('path').dirname(this.dbfile))
-  }
-
-  /**
-   * @method postsave
-   * Cleanup after the save operation is complete.
-   * @param {function} [callback]
-   * An optional callback method to fire after the save is complete.
-   * @private
-   */
-  postsave (callback) {
-    this.emit('save')
-    this.store.emit('save')
-
-    if (NGN.isFn(callback)) {
-      callback()
-    }
-  }
-
-  /**
-   * @method postfetch
-   * A helper method to cleanup after fetching data.
-   * @param {function} [callback]
-   * An optional callback method to fire after the save is complete.
-   * @param {any} content
-   * The content to send to the callback/event handler.
-   * @fires fetch
-   * @private
-   */
-  postfetch (callback, content) {
-    this.emit('fetch', content)
-    this.store.emit('fetch', content)
-
-    if (NGN.isFn(callback)) {
-      callback(content)
-    }
   }
 
   /**
